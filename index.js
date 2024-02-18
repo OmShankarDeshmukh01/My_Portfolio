9cc162e4e954be67ef8c1361a1ac0a057b56ab9e
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2024-02-18 20:44:18 +0530

    updated content

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-
+const path = require("path");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"));
+app.engine("ejs" , ejsMate);
 app.use(express.static(path.join(__dirname , "/public")));
 
 
-app.use("/" ,(req , res)=>{
-    res.send("Hello , I am Root.");
-});
+
 
 
 //Index route
 app.get("/portfolio" , (req , res)=>{
-    
+    res.render("./portfolio/index.ejs");
 });
 
+app.use("/" ,(req , res)=>{
+    res.send("Hello , I am Root.");
+});
 
 app.listen(port , ()=>{
     console.log("app is listening to port");
